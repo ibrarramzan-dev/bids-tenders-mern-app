5819bd4e8610381a1b0ceb1f23f6bc96d8699907
Ibrar Ramzan <ibrarramzan-dev@gmail.com>
2024-07-17 09:42:45 +0500

    search bids update

--- components/SearchResultsBids.jsx
+++ components/SearchResultsBids.jsx
@@ -1,14 +1,35 @@
 import { Montserrat } from "next/font/google";
 import SearchResultsBidsItem from "./SearchResultsBidsItem";
+import { useState } from "react";
+import { Modal } from "antd";
+import ViewBid from "./ViewBid";
 
 const montserrat = Montserrat({ subsets: ["latin"], weight: ["500"] });
 
 export default function SearchResultsBids({ list }) {
+  const [isViewBidModalOpen, setIsViewBidModalOpen] = useState(false);
+  const [activeViewBid, setActiveViewBid] = useState({});
+
+  const onItemClick = (bid) => {
+    console.log("Item bid: ", bid);
+    setActiveViewBid(bid);
+    setIsViewBidModalOpen(true);
+  };
+
   return (
     <div className={`SearchResultsBids ${montserrat.className}`}>
       {list.map((bid) => (
-        <SearchResultsBidsItem bid={bid} />
+        <SearchResultsBidsItem onItemClick={onItemClick} bid={bid} />
       ))}
+
+      <Modal
+        title={<p className="modal-heading">Bid</p>}
+        open={isViewBidModalOpen}
+        onCancel={() => setIsViewBidModalOpen(false)}
+        footer={false}
+      >
+        <ViewBid bid={activeViewBid} />
+      </Modal>
     </div>
   );
 }
--- components/SearchResultsBidsItem.jsx
+++ components/SearchResultsBidsItem.jsx
@@ -1,8 +1,8 @@
-export default function SearchResultsBidsItem({ bid }) {
+export default function SearchResultsBidsItem({ bid, onItemClick }) {
   const { title, description } = bid;
 
   return (
-    <div className="SearchResultsBidsItem">
+    <div onClick={() => onItemClick(bid)} className="SearchResultsBidsItem">
       <p className="SearchResultsBidsItem-title">{title}</p>
       <p>{description}</p>
     </div>
