4ec67ca16e7ed09232de9cd0fa8a827381bb3deb
Ibrar Ramzan <ibrarramzan-dev@gmail.com>
2024-08-03 06:34:43 +0500

    \update

--- app/api/bids/route.js
+++ app/api/bids/route.js
@@ -2,6 +2,24 @@ import connectMongoDB from "@/libs/mongodb";
 import Bid from "@/models/bid";
 import { message } from "antd";
 import { NextResponse } from "next/server";
+import nodemailer from "nodemailer";
+// const nodeMailer = require("nodemailer");
+
+let transportmail = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "ibrarramzan.dev@gmail.com",
+    pass: "twco vshv ljnp hhso",
+  },
+});
+
+let mailContent = {
+  subject: "New Bid Evaluation",
+  text: `<p>Dear [Evaluation Committee member name], 
+  This message serves as a reminder that you have been selected as a member of the evaluation committee for 
+  “Bid Title” with Bid Number: “Bid Number”. To facilitate your access to the necessary evaluation materials, 
+  please use the  following access key: [XXXX] Somali Bids.</p>`,
+};
 
 export async function POST(request) {
   const payload = await request.json();
@@ -11,6 +29,25 @@ export async function POST(request) {
   await connectMongoDB();
 
   await Bid.create(payload);
+
+  /* send email to Evaluation panel members */
+  const { members } = payload;
+  console.log("Members: ", members);
+
+  const recipients = members.map((member) => member.email);
+
+  console.log("recipients: ", recipients);
+
+  transportmail.sendMail(
+    { ...mailContent, to: recipients },
+    function (err, val) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(val.response, "sent Mail...");
+      }
+    }
+  );
 
   return NextResponse.json(
     { message: "Bid created", success: true },
